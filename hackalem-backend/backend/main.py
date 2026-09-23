7c9e866f035c3275e45799737be1e184143c5c82
d3meshka1 <mimamimamimaa@mail.ru>
2026-09-23 14:33:56 +0500

    Update main.py

--- hackalem-backend/backend/main.py
+++ hackalem-backend/backend/main.py
@@ -1,9 +1,41 @@
 from recommender.data_loader import load_contractors
+from recommender.filters import filter_contractors
+from recommender.models import RecommendationRequest
 
 
 contractors = load_contractors()
 
-print("Количество подрядчиков:", len(contractors))
 
-print("\nПервый подрядчик:")
-print(contractors[0])
+request = RecommendationRequest(
+    city="Алматы",
+    date="2026-10-10",
+    event_format="свадьба",
+    category="Флорист",
+    budget=250000,
+    language="русский"
+)
+
+
+results, rejected = filter_contractors(
+    contractors,
+    request
+)
+
+
+print("Всего подрядчиков:", len(contractors))
+
+print("Подходящих:", len(results))
+
+print("\nПричины отказа:")
+print(rejected)
+
+
+print("\nПодходящие подрядчики:")
+
+for contractor in results:
+    print(
+        contractor.anon_name,
+        "-",
+        contractor.price_from_kzt,
+        "₸"
+    )
